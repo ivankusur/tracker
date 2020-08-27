618d26d3125dbeac8b6eae27b3f8cee67874c181
Ivan Kusurhashev <ivankusur@gmail.com>
2020-08-27 19:38:38 +0300

    Create object

---
+++ src/main/java/oop/Dog.java
@@ -0,0 +1,9 @@
+package oop;
+
+public class Dog {
+    public static void main(String[] args) {
+        Dog polkan = new Dog();
+        Dog zuchka = new Dog();
+        Dog sharik = new Dog();
+    }
+}
