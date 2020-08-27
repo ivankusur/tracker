c956c13d20b3b0c9317964f2b8acccbdcd0a7a7d
Ivan Kusurhashev <ivankusur@gmail.com>
2020-08-27 19:50:52 +0300

    call an object method [#323373]

---
+++ src/main/java/oop/Student.java
@@ -0,0 +1,21 @@
+package oop;
+
+public class Student {
+
+    public void music() {
+        System.out.println("tra tra tra");
+    }
+    public void song() {
+        System.out.println("i believe i can fly");
+    }
+
+    public static void main(String[] args) {
+        Student petya  =new Student();
+        petya.music();
+        petya.music();
+        petya.music();
+        petya.song();
+        petya.song();
+        petya.song();
+    }
+}
