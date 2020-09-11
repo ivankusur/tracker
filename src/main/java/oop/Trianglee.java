9aee70a1c4ffc483ea6a104b5b9e5b51cf435028
Ivan Kusurhashev <ivankusur@gmail.com>
2020-09-12 02:39:14 +0300

    Refactoring - Area of a triangle. [# 323368]

--- src/main/java/oop/Trianglee.java
+++ src/main/java/oop/Trianglee.java
@@ -2,37 +2,35 @@ package oop;
 
 import java.awt.*;
 
-public class Triangle {
+public class Trianglee {
     private Point first;
     private Point second;
     private Point third;
 
-    public Triangle(Point ab, Point bc, Point ac) {
+    public Trianglee(Point ab, Point bc, Point ac) {
         this.first = ab;
         this.second = bc;
         this.third = ac;
     }
 
-    public double perimeter(double a, double b, double c) {
-        return first.distance(first) + second.distance(second) + third.distance(third);
+    public double halfPerimeter(double a, double b, double c) {
+        return (a + b + c) / 2;
     }
 
     public boolean exist(double first, double second, double third) {
-        if (first + second > third && second + third > first && third + first > second) {
-            return true;
-        }
+        return first + second > third && second + third > first && third + first > second;
+    }
 
 
-        public double area() {
-            double rsl = -1;
-            double ab = first.distance(second);
-            double ac = first.distance(third);
-            double bc = second.distance(third);
-            double p = period(ab, ac, bc);
-            if (this.exist(ab, ac, bc)) {
-                /* написать формулу для расчета площади треугольника. */
-                rsl = -1;
-            }
+    public double area() {
+        double rsl = -1;
+        double ab = first.distance(second);
+        double ac = first.distance(third);
+        double bc = second.distance(third);
+        double p = halfPerimeter(ab, ac, bc);
+        if (this.exist(ab, ac, bc)) {
+            rsl = Math.sqrt(p * (p - ab) * (p - ac) * (p - bc));
         }
+        return rsl;
     }
 }
