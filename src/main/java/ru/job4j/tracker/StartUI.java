04ee2214dac710d805173f91ce2a81f283fd81b3
Ivan Kusurhashev <ivankusur@gmail.com>
2020-09-17 23:36:32 +0300

    Revert "7. Date. Date display. [# 323381]

---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,15 @@
+package ru.job4j.tracker;
+
+import java.time.format.DateTimeFormatter;
+
+public class StartUI {
+    public static void main(String[] args) {
+        Item time = new Item();
+        time.getCreated();
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        String currentDateTimeFormat = time.getCreated().format(formatter);
+        System.out.println("Текущие дата и время после форматирования: " + currentDateTimeFormat);
+    }
+}
+
