7d2a7406846356821ab5441f677b2aec2fa4e95e
Ivan Kusurhashev <ivankusur@gmail.com>
2020-09-17 23:22:48 +0300

    7. Date. Date display. [# 323381]

--- src/main/java/oop/Machine.java
+++ src/main/java/oop/Machine.java
@@ -3,13 +3,13 @@ package oop;
 import java.util.Arrays;
 
 public class Machine {
-    private final int[] COINS = {10, 5, 2, 1};
+    private final int[] coins = {10, 5, 2, 1};
 
     public int[] change(int money, int price) {
         int[] rsl = new int[100];
         int size = 0;
         int surrender = money - price;
-        for (int coin : COINS) {
+        for (int coin : coins) {
             while (surrender >= coin) {
                 surrender = surrender - coin;
                 rsl[size++] = coin;
