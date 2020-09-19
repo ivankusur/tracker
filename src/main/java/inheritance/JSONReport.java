e4998580e57c528c651a9364caf2b9c630393c1a
Ivan Kusurhashev <ivankusur@gmail.com>
2020-09-19 11:23:21 +0300

    5. @Override annotation [# 323402]FIX

---
+++ src/main/java/inheritance/JSONReport.java
@@ -0,0 +1,8 @@
+package inheritance;
+
+public class JSONReport extends TextReport {
+    @Override
+    public String generate(String name, String body) {
+        return "{" + System.lineSeparator() + "name : " + name + "," + System.lineSeparator() + "body : " + body + System.lineSeparator() + "}";
+    }
+}
