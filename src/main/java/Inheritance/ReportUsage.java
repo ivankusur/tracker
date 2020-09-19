18e4a909fb55fc23ce4b79373c7673dadfc7e0c9
Ivan Kusurhashev <ivankusur@gmail.com>
2020-09-19 10:01:00 +0300

    4. Override [# 323401]

---
+++ src/main/java/Inheritance/ReportUsage.java
@@ -0,0 +1,9 @@
+package Inheritance;
+
+public class ReportUsage {
+    public static void main(String[] args) {
+        JSONReport report = new JSONReport();
+        String text = report.generate("name", "body");
+        System.out.println(text);
+    }
+}
---
+++ src/main/java/Inheritance/TextReport.java
@@ -0,0 +1,7 @@
+package Inheritance;
+
+public class TextReport {
+    public String generate(String name, String body) {
+        return name + System.lineSeparator() + body;
+    }
+}
