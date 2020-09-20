d22bd19f8cd173542c0f4cf65ab13c90a73f8cd6
Ivan Kusurhashev <ivankusur@gmail.com>
2020-09-20 23:55:09 +0300

    3.1. Array with empty cells [# 323411]

---
+++ src/main/java/pojo/Shop.java
@@ -0,0 +1,21 @@
+package pojo;
+
+public class Shop {
+    public static int indexOfNull(Product[] products) {
+        int rsl = -1;
+        for (int i = 0; i < products.length; i++)
+            if (products[i] == null) {
+                rsl = i;
+                break;
+            }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        Product[] goods = new Product[5];
+        goods[0] = new Product("Milk", 10);
+        goods[1] = new Product("Bread", 4);
+        goods[2] = new Product("Egg", 19);
+        System.out.println("Index of first element with NULL in array is : " + Shop.indexOfNull(goods));
+    }
+}
