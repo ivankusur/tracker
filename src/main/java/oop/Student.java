03b10e24c9f451667b67333ce1f7a1c153d6fe0f
Ivan Kusurhashev <ivankusur@gmail.com>
2020-12-17 23:13:00 +0200

    CheckStyle change

--- src/main/java/oop/Student.java
+++ src/main/java/oop/Student.java
@@ -5,12 +5,13 @@ public class Student {
     public void music() {
         System.out.println("tra tra tra");
     }
+
     public void song() {
         System.out.println("i believe i can fly");
     }
 
     public static void main(String[] args) {
-        Student petya  =new Student();
+        Student petya = new Student();
         petya.music();
         petya.music();
         petya.music();
