2481e07721d88577a9ca476359976e90d7416e4b
Ivan Kusurhashev <ivankusur@gmail.com>
2020-12-17 22:19:18 +0200

    4. Comparison of models. Equals method [# 323410]

--- src/main/java/oop/Battery.java
+++ src/main/java/oop/Battery.java
@@ -9,14 +9,14 @@ public class Battery {
 
     public void exchange(Battery another) {
         another.load = this.load + another.load;
-        this.load -= this.load;
+        this.load = 0;
     }
 
     public static void main(String[] args) {
         Battery cellphone = new Battery(1000);
         Battery powerBank = new Battery(2000);
         powerBank.exchange(cellphone);
-        System.out.println(powerBank.load);
-        System.out.println(cellphone.load);
+        System.out.println("PowerBank Load:" + powerBank.load);
+        System.out.println("CellPhone Load:" + cellphone.load);
     }
 }
