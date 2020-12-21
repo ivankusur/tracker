cb4cd347ae63b51ab603c7c43c25114f69297d20
Ivan Kusurhashev <ivankusur@gmail.com>
2020-12-21 10:37:25 +0200

    5. Tracker - storage [# 323416] Fix method "findByName".

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -31,13 +31,15 @@ public class Tracker {
 
     public Item[] findByName(String key) {
         Item[] rsl = new Item[size];
+        int counter = 0;
         for (int i = 0; i < size; i++) {
             Item item = items[i];
-            if (item.toString().equals(key)) {
+            if (item.getName().equals(key)) {
                 rsl[i] = item;
             }
+            counter++;
         }
-        return rsl;
+        return Arrays.copyOf(rsl, counter);
     }
 
     public Item[] findAll() {
