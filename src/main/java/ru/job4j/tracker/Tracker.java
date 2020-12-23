90eb6879254349c14cf99707ca436162ae54b234
Ivan Kusurhashev <ivankusur@gmail.com>
2020-12-23 12:32:29 +0200

    7. How to delete an order Tracker.delete [# 323413] Fix method "delete".

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -57,17 +57,15 @@ public class Tracker {
     }
 
 
-
     public boolean delete(int id) {
         int index = indexOf(id);
         if (index == -1) {
             return false;
-        } else {
-            items[index] = null;
-            System.arraycopy(items, index + 1, items, index, size - index);
-            items[size - 1] = null;
-            size--;
-            return true;
         }
+        items[index] = null;
+        System.arraycopy(items, index + 1, items, index, size - index);
+        items[size - 1] = null;
+        size--;
+        return true;
     }
 }
