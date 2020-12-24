c13cb8a9f4a2acb8c91450986fec76a74cfd3da3
Ivan Kusurhashev <ivankusur@gmail.com>
2020-12-24 12:03:09 +0200

    2. Scanner and reading a number from the console. [# 323337], fixed.

--- src/main/java/ru/job4j/io/ElevenMatch.java
+++ src/main/java/ru/job4j/io/ElevenMatch.java
@@ -5,34 +5,31 @@ import java.util.Scanner;
 public class ElevenMatch {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        boolean run = true;
+        boolean player = true;
         int count = 11;
         while (count > 0) {
-            System.out.println("Player 1 take a matches (1,2, or 3)");
-            int selectFirstPlayer = Integer.valueOf(input.nextLine());
-            while (selectFirstPlayer < 1 || selectFirstPlayer > 3) {
+            if (player) {
+                System.out.println("First player take a matches (1,2, or 3)");
+            } else {
+                System.out.println("Second player take a matches (1,2, or 3)");
+            }
+            int select = Integer.valueOf(input.nextLine());
+            while (select < 1 || select > 3 || select > count) {
                 System.out.println("Take correct number of matches (1,2, or 3)");
-                selectFirstPlayer = Integer.valueOf(input.nextLine());
+                select = Integer.valueOf(input.nextLine());
             }
-            count -= selectFirstPlayer;
+            count -= select;
             System.out.println(count + " matches left");
-            if (count == 0) {
-                System.out.println("Player 1 WIN");
-                break;
-            }
-            System.out.println("Player 2 take a matches (1,2, or 3)");
-            int selectSecondPlayer = Integer.valueOf(input.nextLine());
-            while (selectSecondPlayer < 1 || selectSecondPlayer > 3) {
-                System.out.println("Take correct number of matches (1,2, or 3)");
-                selectSecondPlayer = Integer.valueOf(input.nextLine());
-            }
-            count -= selectSecondPlayer;
-            System.out.println(count + " matches left");
-            if (count == 0) {
-                System.out.println("Player 2 WIN");
-                break;
-            }
+            player = !player;
+        }
+        if (player) {
+            System.out.println("Second player WIN");
+        } else {
+            System.out.println("First player WIN");
         }
     }
 }
+
+
+
 
