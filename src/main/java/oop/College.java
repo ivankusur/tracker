4f523565a5d9795ee11fe98407ebdb871cb16e4e
Ivan Kusurhashev <ivankusur@gmail.com>
2020-12-25 21:32:40 +0200

    3.1. Cast. Type ups and downs. [# 323341]

---
+++ src/main/java/oop/College.java
@@ -0,0 +1,9 @@
+package oop;
+
+public class College {
+    public static void main(String[] args) {
+        FreshMan freshMan = new FreshMan();
+        Students std = freshMan;
+        Object obj = freshMan;
+    }
+}
