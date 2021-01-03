3d9dec46b6f1702db6137cd3a12f4647bdc54d6b
Ivan Kusurhashev <ivankusur@gmail.com>
2021-01-03 19:59:50 +0200

    0.4. Пользовательские исключения. [#323358]

---
+++ src/main/java/ru/job4j/exception/FindEl.java
@@ -0,0 +1,25 @@
+package ru.job4j.exception;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int index = 0; index < value.length; index++) {
+            if (value[index].equals(key)) {
+                rsl = index;
+            }
+            if (rsl == -1) {
+                throw new ElementNotFoundException("Element " + key + " is absent");
+            }
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) throws ElementNotFoundException {
+        String[] value = {"a", "b", "c"};
+        try {
+            indexOf(value, "abc");
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
