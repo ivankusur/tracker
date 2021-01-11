53a22ca9a076a407dfeb6b9ff55b2d52e9d59622
Ivan Kusurhashev <ivankusur@gmail.com>
2021-01-11 23:36:21 +0200

    2. Singleton. [#323390], remove modifier static.

--- src/main/java/ru/job4j/singleton/TrackerSingleEnum.java
+++ src/main/java/ru/job4j/singleton/TrackerSingleEnum.java
@@ -5,9 +5,9 @@ import ru.job4j.tracker.Tracker;
 public enum TrackerSingleEnum {
     INSTANCE;
 
-    private static Tracker singleTracker;
+    private final Tracker singleTracker = new Tracker();
 
-    public static Tracker getSingleTracker() {
+    public Tracker getSingleTracker() {
         return singleTracker;
     }
 
