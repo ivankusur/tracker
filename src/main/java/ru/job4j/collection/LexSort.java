e525b1e156acc3f1af82806c0f74e44635589d97
Ivan Kusurhashev <ivankusur@gmail.com>
2021-01-25 13:09:58 +0200

    4. Сортировка номера [#323310], fixed.

--- src/main/java/ru/job4j/collection/LexSort.java
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -1,44 +1,14 @@
 package ru.job4j.collection;
 
-
-
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Comparator;
-import java.util.List;
 
 public class LexSort implements Comparator<String> {
 
     @Override
     public int compare(String left, String right) {
-        List<Integer> firstList = new ArrayList<>();
-        List<Integer> secondList = new ArrayList<>();
-        String[] leftStrings = left.split("");
-        for (String s : leftStrings) {
-            if (s.matches("[-+]?\\d+")) {
-               firstList.add(Integer.parseInt(s));
-            }
-        }
-        String[] rightStrings = right.split("");
-        for (String s : rightStrings) {
-            if (s.matches("[-+]?\\d+")) {
-                secondList.add(Integer.parseInt(s));
-            }
-        }
-        Integer[] firstInt = firstList.toArray(new Integer[0]);
-        Integer[] secondInt = secondList.toArray(new Integer[0]);
-        return Arrays.compare(firstInt, secondInt);
-    }
-
-    public static void main(String[] args) {
-        String[] input = {
-                "10. Task.",
-                "1. Task.",
-                "2. Task."
-        };
-        Arrays.sort(input, new LexSort());
-        for (String s : input) {
-            System.out.println(s);
-        }
+        String[] leftStrings = left.split("\\.");
+        String[] rightStrings = right.split("\\.");
+        return Integer.compare(Integer.parseInt(leftStrings[0]),
+                Integer.parseInt(rightStrings[0]));
     }
 }
